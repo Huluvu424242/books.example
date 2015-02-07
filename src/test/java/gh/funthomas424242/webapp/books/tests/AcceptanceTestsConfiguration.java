beaa4cc836fde6c10ac5138fa63377e03010b057
Thomas Schubert <funthomas424242@gmail.com>
2015-02-07 20:53:02 +0100

    bdd tests configureation fixed
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

--- src/test/java/gh/funthomas424242/webapp/books/tests/AcceptanceTestsConfiguration.java
+++ src/test/java/gh/funthomas424242/webapp/books/tests/AcceptanceTestsConfiguration.java
@@ -1,10 +1,14 @@
 package gh.funthomas424242.webapp.books.tests;
 
+import gh.funthomas424242.webapp.books.BookController;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 //@Import({DomainConfiguration.class, InfrastructureConfiguration.class})
+@Import ({BookController.class})
 @ComponentScan
 public class AcceptanceTestsConfiguration {
 }
