ec20ac62df7248b3923e6dad47ac2cd86a4e8b1a
Thomas Schubert <funthomas424242@gmail.com>
2015-02-27 19:12:24 +0100

    testfälle korrigiert.

--- books.app/src/test/java/gh/funthomas424242/webapp/books/web/GuiIntegrationTest.java
+++ books.app/src/test/java/gh/funthomas424242/webapp/books/web/GuiIntegrationTest.java
@@ -1,16 +1,18 @@
 package gh.funthomas424242.webapp.books.web;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import gh.funthomas424242.webapp.books.Application;
 
 import java.io.IOException;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,9 +20,19 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 
+/**
+ * @author huluvu424242
+ * 
+ *         Fixer GUI Test der bei jedem Modultest mitläuft. Soll extrem kurz
+ *         gehalten werden und nur grundlegende Dinge sicherstellen. Beispiele:
+ *         - Der Server startet - Die Startseite wird ausgeliefert. - Das
+ *         JavaScript arbeitet korrekt (ein dyn. Text muss erkannt werden).
+ * 
+ * 
+ * 
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = { Application.class })
 @WebIntegrationTest({ "server.port=0" })
@@ -43,70 +55,64 @@ public class GuiIntegrationTest {
 	@Autowired
 	protected WebApplicationContext context;
 
-	protected WebDriver driver;
-	// protected SeleniumPage page;
+	static protected WebDriver driver;
 
-	protected WebDriver getWebDriver() {
-		return this.driver;
+	protected static WebDriver getWebDriver() {
+		return driver;
 	}
 
-	@Before
-	public void setup() throws IOException {
+	@BeforeClass
+	public static void setup() throws IOException {
 		driver = new HtmlUnitDriver(true);
 	}
 
-	@After
-	public void destroy() {
+	@AfterClass
+	public static void destroy() {
 		if (driver != null) {
 			driver.close();
 		}
 	}
 
 	@Test
-	public void seleniumTest() {
-		logInfo("DRIVER:" + driver);
-		logInfo(SERVER_URL + serverPort + LINK_BOOKS);
+	public void startSeiteGeliefertJavaScriptArbeitet() {
 		driver.get(SERVER_URL + serverPort + LINK_BOOKS);
-		logInfo(SERVER_URL + serverPort + LINK_BOOKS);
-		logInfo("TITEL:" + driver.getTitle());
-		logInfo("TITEL:" + driver.getPageSource());
-
+		assertEquals("Kleine Bücherverwaltung - Bücherregal", driver.getTitle());
+		final WebElement element = driver.findElement(By.id("message"));
+		assertEquals("Aktuell keine Bücher im Buchregal.", element.getText());
 	}
 
 	@Test
-	@Ignore
 	public void homePage() {
 		final TestRestTemplate restTemplate = new TestRestTemplate();
 
-		logInfo(MESSAGE_LEERES_REGAL);
+		// logInfo(MESSAGE_LEERES_REGAL);
 		final String pageContent = restTemplate.getForObject(SERVER_URL
 				+ serverPort + LINK_BOOKS, String.class);
-		logInfo(pageContent);
-		assertTrue(pageContent.contains(MESSAGE_LEERES_REGAL));
+		// logInfo(pageContent);
+		assertTrue(pageContent.contains("Kleine Bücherverwaltung - Bücherregal"));
 	}
 
-	protected void logInfo(String text) {
-		System.out.println("=============================");
-		System.out.println("LOG-INFO:" + text);
-		System.out.println("=============================");
-	}
-
-	@Test
-	@Ignore
-	public void addPage() {
-		RestTemplate restTemplate = new RestTemplate();
-		assertTrue(restTemplate.getForObject(
-				SERVER_URL + serverPort + LINK_BUCH_ERFASSEN, String.class)
-				.contains("Aktuell keine Bücher im Buchregal."));
-	}
-
-	@Test
-	@Ignore
-	public void health() {
-		RestTemplate restTemplate = new RestTemplate();
-		assertTrue(restTemplate.getForObject(
-				SERVER_URL + serverPort + "/health", String.class).contains(
-				"UP"));
-	}
-
+	// @Test
+	// @Ignore
+	// public void addPage() {
+	// RestTemplate restTemplate = new RestTemplate();
+	// assertTrue(restTemplate.getForObject(
+	// SERVER_URL + serverPort + LINK_BUCH_ERFASSEN, String.class)
+	// .contains("Aktuell keine Bücher im Buchregal."));
+	// }
+	//
+	// @Test
+	// @Ignore
+	// public void health() {
+	// RestTemplate restTemplate = new RestTemplate();
+	// assertTrue(restTemplate.getForObject(
+	// SERVER_URL + serverPort + "/health", String.class).contains(
+	// "UP"));
+	// }
+	//
+	// protected void logInfo(String text) {
+	// System.out.println("=============================");
+	// System.out.println("LOG-INFO:" + text);
+	// System.out.println("=============================");
+	// }
 }
