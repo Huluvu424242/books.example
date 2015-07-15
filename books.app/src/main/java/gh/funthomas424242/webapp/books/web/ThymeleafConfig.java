601ac28a8d012cff1c3cfc89bb976ba0ebc414d9
Thomas Schubert <funthomas424242@gmail.com>
2015-07-15 21:13:59 +0200

    angular-js ui grid added
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

---
+++ books.app/src/main/java/gh/funthomas424242/webapp/books/web/ThymeleafConfig.java
@@ -0,0 +1,29 @@
+//package gh.funthomas424242.webapp.books.web;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.support.ResourceBundleMessageSource;
+//import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+//
+//@Configuration
+//public class ThymeleafConfig {
+//
+//@Bean
+//public ServletContextTemplateResolver defaultTemplateResolver() {
+//    ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+//    resolver.setPrefix("/templates/");
+//    resolver.setSuffix(".html");
+//    resolver.setTemplateMode("LEGACYHTML5");
+//    resolver.setCacheable(false);
+//    return resolver;
+//}
+//
+//@Bean
+//public ResourceBundleMessageSource messageSource() {
+//    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//    messageSource.setBasename("messages");
+//    messageSource.setDefaultEncoding("UTF-8");
+//
+//    return messageSource;
+//}
+//}
