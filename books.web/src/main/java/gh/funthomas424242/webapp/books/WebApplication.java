073b9f64d6f9068793e991a4f5f6ed9a9d58eda8
FunThomas424242 <funthomas424242@gmail.com>
2015-10-25 22:10:01 +0000

    application web renamed
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- books.web/src/main/java/gh/funthomas424242/webapp/books/WebApplication.java
+++ books.web/src/main/java/gh/funthomas424242/webapp/books/WebApplication.java
@@ -2,7 +2,7 @@ package gh.funthomas424242.webapp.books;
 
 /*
  * #%L
- * Books.App
+ * Books.App - Web
  * %%
  * Copyright (C) 2015 Pivotal Software, Inc.
  * %%
@@ -22,23 +22,16 @@ package gh.funthomas424242.webapp.books;
  * #L%
  */
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class Application {
+public class WebApplication {
 
 	public static void main(String[] args) {
-		final SpringApplication app = new SpringApplication(Application.class);
+		final SpringApplication app = new SpringApplication(WebApplication.class);
 	    app.setShowBanner(true);
-	    final Map<String,Object> config=new HashMap<String,Object>();
-	    config.put("spring.datasource.url","jdbc:h2:file:~/bookdb;DB_CLOSE_ON_EXIT=FALSE");
-	    config.put("spring.jpa.hibernate.ddl-auto", "update");
-	    app.setDefaultProperties(config);
 	    app.run(args);
 	}
 }
