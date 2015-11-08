d15e37ee24c3a0a7752935f6c1f0bad988c79c1e
Thomas Schubert <funthomas424242@gmail.com>
2015-11-08 18:42:01 +0100

    Datenbank h2 in eigenen Docker Container ausgelagert
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

--- startSingleDockerArch.sh
+++ startSingleDockerArch.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+docker rm -f books_db
+docker run -d -p 1521:1521 --name books_db funthomas424242/books.db
+
 docker rm -f books_app
-docker run -d -p 8080:8080  --name books_app funthomas424242/books.app
+docker run -d -p 8080:8080 --link books_db:localhost --name books_app funthomas424242/books.app
 
 docker rm -f books_web
 docker run -d -p 80:9000  --link books_app:localhost --name books_web funthomas424242/books.web
