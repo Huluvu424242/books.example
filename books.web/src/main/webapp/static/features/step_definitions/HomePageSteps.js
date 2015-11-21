a99d17e9e62cb5039b67451f4f588e10e6b7f136
Thomas Schubert <funthomas424242@gmail.com>
2015-11-22 00:41:48 +0100

    lokale Änderungen eingecheckt
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

---
+++ books.web/src/main/webapp/static/features/step_definitions/HomePageSteps.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var assert = require('assert');
+
+
+var steps = function() {
+    
+    var page = require('./HomePage.js');//page object
+    
+    
+    this.Given(/^I am on the homepage$/, function(done) {
+	page.navigate();
+	done();
+    });
+    
+    this.Then(/^I should see a "([^"]*)" link$/, function(link, callback) {
+	support.findByBinding(this, link, function(result){
+	    result.getText().then (function(text){
+		text.trim().toLowerCase().should.equal(link.trim().toLowerCase());             
+		setTimeout(callback, 1000);
+	    });     
+	});
+    });
+    
+    this.Then(/^I should not see a "([^"]*)" link$/, function(link, callback) {
+	support.isElementPresent(this, link, function(result){
+	    result.should.equal(false);
+	    setTimeout(callback, 1000);
+	});
+    });
+    
+    this.Then(/^I should see a "([^"]*)"$/, function(link, callback) {
+	support.isElementPresentByClass(this, link, function(result){
+	    result.should.equal(true);
+	    setTimeout(callback, 1000);
+	});
+    });
+    
+};
+
+module.exports = steps;
