ae1b4703cfcf82dfb74f59ffb761f972dc3a781c
Thomas Schubert <funthomas424242@gmail.com>
2015-11-29 21:53:38 +0100

    before und after hooks eingeführt
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

---
+++ books.web/src/main/webapp/static/features/support/hooks.js
@@ -0,0 +1,46 @@
+
+
+var myHooks = function() {
+
+    var fs = require('fs');
+    var encoder = require('base64-js').fromByteArray;
+    var decoder = require('base64-js').toByteArray;
+    
+    this.Before(function(scenario) {
+        console.log("BEFORE SCENARIO:" + scenario.isFailed());
+        // Just like inside step definitions, "this" is set to a World instance.
+        // It's actually the same instance the current scenario step definitions
+        // will receive.
+
+        // Let's say we have a bunch of "maintenance" methods available on our
+        // World
+        // instance, we can fire some to prepare the application for the next
+        // scenario:
+
+        // this.bootFullTextSearchServer();
+        // this.createSomeUsers();
+    });
+
+    this.After(function(scenario, callback) {
+
+        console.log("AFTER SCENARIO:" + scenario.isFailed());
+
+        if (scenario.isFailed()) {
+            
+            browser.takeScreenshot().then(function(png) {
+                scenario.attach(new Buffer(png, 'base64'), 'image/png'); 
+                
+//               // png = decoder(png);
+//                fs.writeFile('screenshot.png', png, 'binary', function (err)
+//                {
+//                    if (err) throw err
+//                    console.log('Screenshot saved.')
+//                })
+            });
+        }
+
+    });
+
+};
+
+module.exports = myHooks;
