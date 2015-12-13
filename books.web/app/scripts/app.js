2c094c655c87c0f5aa1bf27c551f999ade123916
Thomas Schubert <funthomas424242@gmail.com>
2015-12-13 18:34:36 +0100

    automatische yo config

---
+++ books.web/app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name bookswebApp
+ * @description
+ * # bookswebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('bookswebApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ books.web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bookswebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bookswebApp
+ */
+angular.module('bookswebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
