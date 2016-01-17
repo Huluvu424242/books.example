d2fa27d5495e6ccaaf4de7fc8a4438e4e0c7c372
Thomas Schubert <funthomas424242@gmail.com>
2016-01-17 23:54:48 +0100

    JSLint Fehler beseitigt mit der brackets IDE

--- books.web/app/scripts/controllers/BookController.js
+++ books.web/app/scripts/controllers/BookController.js
@@ -21,97 +21,98 @@
  */
 'use strict';
 angular.module('BooksApp')
-.controller('BookController', ['$http', '$timeout', '$interval', 
-function($http, $timeout, $interval) {
+	.controller('BookController', ['$http', '$timeout', '$interval',
+		function ($http, $timeout, $interval) {
 
-	var bc = this;
+			var bc = this;
 
-	bc.editModusAktiv = false;
-	bc.books = [];
+			bc.editModusAktiv = false;
+			bc.books = [];
 
-	//GET http://localhost:8080/books
-	bc.refreshData = function(defaultURL) {
+			//GET http://localhost:8080/books
+			bc.refreshData = function (defaultURL) {
 
-		//default für allerersten Aufruf
-		if ( typeof (bc.selfURL) === 'undefined'){
-			bc.selfURL = defaultURL;
+            //default für allerersten Aufruf
+                if (typeof (bc.selfURL) === 'undefined') {
+                    bc.selfURL = defaultURL;
+                }
+
+				bc.books = [];
+
+				$http.get(bc.selfURL).then(function erfolg(response) {
+
+					bc.baseURL = response.data.baseURL;
+					bc.selfURL = response.data.selfURL;
+					bc.newURL = response.data.newURL;
+					bc.nextURL = response.data.nextURL;
+					bc.prevURL = response.data.prevURL;
+
+					response.data.resources.forEach(function (book) {
+						bc.books.push(book);
+					});
+					// Ausblenden der Tabelle wenn keine Treffer
+					bc.ergebnisseVorhanden = (bc.books.length > 0);
+				}, function fehler(response) {
+					bc.message = response.statusText;
+				});
+			};
+
+			//POST http://localhost:8080/book/new
+			bc.addBook = function () {
+
+				var titel = bc.newBookData.titel,
+				    isbn = bc.newBookData.isbn;
+
+				$http({
+					method: 'POST',
+					//url : bc.newURL,
+					url: 'http://localhost:8080/book/new',
+					params: {
+						'titel': titel,
+						'isbn': isbn
+					}
+				}).then(function erfolg(response) {
+					bc.newBookData = {};
+					bc.refreshData();
+				}, function fehler(response) {
+					bc.message = "FEHLER BEIM ANGULAR ADD";
+				});
+			};
+
+			//DELETE http://localhost:8080/book/{id}
+			bc.deleteBook = function (url, index) {
+
+				$http.delete(url).success(function (result) {
+					//console.log(result);
+					//document.getElementById(url).remove();
+					bc.refreshData();
+				}).error(function () {
+					console.log("FEHLER BEIM ANGULAR DELETE");
+				});
+			};
+
+			bc.refreshData('http://localhost:8080/books');
 		}
-
-		bc.books = [];
-
-		$http.get(bc.selfURL).then(function erfolg(response) {
-
-			bc.baseURL = response.data.baseURL;
-			bc.selfURL = response.data.selfURL;
-			bc.newURL = response.data.newURL;
-			bc.nextURL = response.data.nextURL;
-			bc.prevURL = response.data.prevURL;
-
-			response.data.resources.forEach(function(book) {
-				bc.books.push(book);
-			});
-			// Ausblenden der Tabelle wenn keine Treffer
-			bc.ergebnisseVorhanden = (bc.books.length > 0);
-		}, function fehler(response) {
-			bc.message = response.statusText;
-		});
-	};
-
-	//POST http://localhost:8080/book/new
-	bc.addBook = function() {
-
-		var titel = bc.newBookData.titel;
-		var isbn = bc.newBookData.isbn;
-
-		$http({
-			method : 'POST',
-			//url : bc.newURL,
-			url : 'http://localhost:8080/book/new',
-			params : {
-				'titel' : titel,
-				'isbn' : isbn
-			}
-		}).then(function erfolg(response) {
-			bc.newBookData = {};
-			bc.refreshData();
-		}, function fehler(response) {
-			bc.message = "FEHLER BEIM ANGULAR ADD";
-		});
-	};
-
-	//DELETE http://localhost:8080/book/{id}
-	bc.deleteBook = function(url, index) {
-
-		$http.delete(url).success(function(result) {
-			//console.log(result);
-			//document.getElementById(url).remove();
-			bc.refreshData();
-		}).error(function() {
-			console.log("FEHLER BEIM ANGULAR DELETE");
-		});
-	};
-
-	bc.refreshData('http://localhost:8080/books');
-}]);
+	    ]);
 
 // if ( typeof NS == 'undefined') {
-	// NS = {};
+// NS = {};
 // }
-// 
+//
 // NS.myFunction = {
-	// //empty stuff array, filled during initialization
-	// stuff : [],
-// 
-	// init : function init() {
-		// this.stuff.push('Testing');
-	// },
-	// reset : function reset() {
-		// this.stuff = [];
-	// },
-	// //replace “//will add new functionality here later” with the following:
-	// append : function append(string1, string2) {
-		// return string1 + ' ' + string2;
-	// }
+// //empty stuff array, filled during initialization
+// stuff : [],
+//
+// init : function init() {
+// this.stuff.push('Testing');
+// },
+// reset : function reset() {
+// this.stuff = [];
+// },
+// //replace “//will add new functionality here later” with the following:
+// append : function append(string1, string2) {
+// return string1 + ' ' + string2;
+// }
 // };
-// 
-// NS.myFunction.init(); 
+//
+// NS.myFunction.init();
