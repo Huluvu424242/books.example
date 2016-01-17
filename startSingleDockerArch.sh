8bf5e0020ba6da7451fb8e5ac0126c12543f0733
FunThomas424242 <funthomas424242@gmail.com>
2016-01-17 22:08:54 +0100

    logging angepasst weil docker mit root permission schreibt
    docker images loggen jetzt unter /tmp/rootlogs
    jars loggen jetzt unter /tmp/logs
    
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- startSingleDockerArch.sh
+++ startSingleDockerArch.sh
@@ -3,8 +3,8 @@
 #docker run -d -p 1521:1521 -p 80:80 --name books_db funthomas424242/books.db
 
 docker rm -f books_app
-docker run -d --name books_app -p 8080:8080 -v /tmp:/tmp --link books_db:books_db  funthomas424242/books.app
+docker run -d --name books_app -p 8080:8080 -v /tmp/rootlogs:/tmp --link books_db:books_db  funthomas424242/books.app
 
 docker rm -f books_web
-docker run -d --name books_web -p 9000:9000 -v /tmp:/tmp --link books_app:localhost  funthomas424242/books.web
+docker run -d --name books_web -p 9000:9000 -v /tmp/rootlogs:/tmp --link books_app:localhost  funthomas424242/books.web
 
