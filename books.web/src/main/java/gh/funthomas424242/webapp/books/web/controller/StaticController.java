e739feb60ef1f323f790808881cbd4250b961493
FunThomas424242 <funthomas424242@gmail.com>
2016-01-17 21:08:19 +0100

    webapp prepared for docker container
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

---
+++ books.web/src/main/java/gh/funthomas424242/webapp/books/web/controller/StaticController.java
@@ -0,0 +1,15 @@
+// package gh.funthomas424242.webapp.books.web.controller;
+//
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
+//
+// @Controller
+// public class StaticController {
+//
+// @RequestMapping(value = "/", method = RequestMethod.GET)
+// public String landingPage() {
+// return "/index.html";
+// }
+//
+// }
