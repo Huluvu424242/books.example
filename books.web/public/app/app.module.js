bd0d78a71ad08ce77cd33c0945abe21bb24eb504
FunThomas424242 <funthomas424242@gmail.com>
2016-01-29 23:31:47 +0100

    app.js in modul definition und routing aufgeteilt

---
+++ books.web/public/app/app.module.js
@@ -0,0 +1,24 @@
+(function () {
+
+    'use strict';
+
+    /**
+     * @ngdoc overview
+     * @name bookswebApp
+     * @description
+     * # bookswebApp
+     *
+     * Main module of the application.
+     */
+    angular.module('BooksApp', [
+        'ngAnimate',
+        'ngAria',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ]);
+
+}());
---
+++ books.web/public/app/app.routes.js
@@ -0,0 +1,46 @@
+(function () {
+
+    'use strict';
+
+    /**
+     * @ngdoc overview
+     * @name bookswebApp
+     * @description
+     * # bookswebApp
+     *
+     * Main module of the application.
+     */
+    angular.module('BooksApp')
+        .config(['$routeProvider', function ($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'app/components/book/booklist.html',
+                    controller: 'BookController',
+                    controllerAs: 'bc'
+                })
+                .when('/einstellungen', {
+                    templateUrl: 'app/einstellungen.html',
+                    controller: 'DashboardController',
+                    controllerAs: 'options'
+                })
+                .when('/impressum', {
+                    templateUrl: 'app/impressum.html',
+                    controller: 'DashboardController',
+                    controllerAs: 'impressum'
+                })
+                .when('/user', {
+                    templateUrl: 'app/user.html',
+                    controller: 'DashboardController',
+                    controllerAs: 'user'
+                })
+                .when('/about', {
+                    templateUrl: 'app/about.html',
+                    controller: 'DashboardController',
+                    controllerAs: 'about'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+        }]);
+
+}());
