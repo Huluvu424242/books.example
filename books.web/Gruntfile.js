c310b540f642b7ffd460a62649d53ef1aae130f3
FunThomas424242 <funthomas424242@gmail.com>
2016-01-31 20:15:33 +0100

    Testkonfiguration für karma erstellt

--- books.web/Gruntfile.js
+++ books.web/Gruntfile.js
@@ -1,7 +1,5 @@
-// Generated on 2015-12-12 using generator-angular 0.15.1
 (function () {
     'use strict';
-
 
     // # Globbing
     // for performance reasons we're only matching one level down:
@@ -15,8 +13,8 @@
         // npm config
         grunt.file.readJSON('package.json');
 
-        // Time how long tasks take. Can help when optimizing build times
-        require('time-grunt')(grunt);
+        //
+        grunt.loadNpmTasks('grunt-jscs');
 
         // Automatically load required Grunt tasks
         require('jit-grunt')(grunt, {
@@ -35,45 +33,44 @@
         // Define the configuration for all the tasks
         grunt.initConfig({
 
-
             // Project settings
             yeoman: appConfig,
 
             // Watches files for changes and runs tasks based on the changed files
-            watch: {
-                bower: {
-                    files: ['bower.json'],
-                    tasks: ['wiredep']
-                },
-                js: {
-                    files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
-                    tasks: ['newer:jshint:all', 'newer:jscs:all'],
-                    options: {
-                        livereload: '<%= connect.options.livereload %>'
-                    }
-                },
-                jsTest: {
-                    files: ['test/spec/{,*/}*.js'],
-                    tasks: ['newer:jshint:test', 'newer:jscs:test', 'karma']
-                },
-                // compass: {
-                // files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
-                // tasks: ['compass:server', 'postcss:server']
-                // },
-                gruntfile: {
-                    files: ['Gruntfile.js']
-                },
-                livereload: {
-                    options: {
-                        livereload: '<%= connect.options.livereload %>'
-                    },
-                    files: [
-                        '<%= yeoman.app %>/{,*/}*.html',
-                        '.tmp/styles/{,*/}*.css',
-                        '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
-                    ]
-                }
-            },
+            //            watch: {
+            //                bower: {
+            //                    files: ['bower.json'],
+            //                    tasks: ['wiredep']
+            //                },
+            //                js: {
+            //                    files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
+            //                    tasks: ['newer:jshint:all', 'newer:jscs:all'],
+            //                    options: {
+            //                        livereload: '<%= connect.options.livereload %>'
+            //                    }
+            //                },
+            //                jsTest: {
+            //                    files: ['test/spec/{,*/}*.js'],
+            //                    tasks: ['newer:jshint:test', 'newer:jscs:test', 'karma']
+            //                },
+            //                // compass: {
+            //                // files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
+            //                // tasks: ['compass:server', 'postcss:server']
+            //                // },
+            //                gruntfile: {
+            //                    files: ['Gruntfile.js']
+            //                },
+            //                livereload: {
+            //                    options: {
+            //                        livereload: '<%= connect.options.livereload %>'
+            //                    },
+            //                    files: [
+            //                        '<%= yeoman.app %>/{,*/}*.html',
+            //                        '.tmp/styles/{,*/}*.css',
+            //                        '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+            //                    ]
+            //                }
+            //            },
 
             // The actual grunt server settings
             /*connect: {
@@ -155,7 +152,7 @@
                 all: {
                     src: [
                         'Gruntfile.js',
-                        '<%= yeoman.app %>/scripts/{,*/}*.js'
+                        '<%= yeoman.app %>/app/{,*/}*.js'
                     ]
                 },
                 test: {
@@ -179,34 +176,34 @@
             },
 
             // Add vendor prefixed styles
-            postcss: {
-                options: {
-                    processors: [
-                        require('autoprefixer-core')({
-                            browsers: ['last 1 version']
-                        })
-                    ]
-                },
-                server: {
-                    options: {
-                        map: true
-                    },
-                    files: [{
-                        expand: true,
-                        cwd: '.tmp/styles/',
-                        src: '{,*/}*.css',
-                        dest: '.tmp/styles/'
-                    }]
-                },
-                dist: {
-                    files: [{
-                        expand: true,
-                        cwd: '.tmp/styles/',
-                        src: '{,*/}*.css',
-                        dest: '.tmp/styles/'
-                    }]
-                }
-            },
+            //            postcss: {
+            //                options: {
+            //                    processors: [
+            //                        require('autoprefixer-core')({
+            //                            browsers: ['last 1 version']
+            //                        })
+            //                    ]
+            //                },
+            //                server: {
+            //                    options: {
+            //                        map: true
+            //                    },
+            //                    files: [{
+            //                        expand: true,
+            //                        cwd: '.tmp/styles/',
+            //                        src: '{,*/}*.css',
+            //                        dest: '.tmp/styles/'
+            //                    }]
+            //                },
+            //                dist: {
+            //                    files: [{
+            //                        expand: true,
+            //                        cwd: '.tmp/styles/',
+            //                        src: '{,*/}*.css',
+            //                        dest: '.tmp/styles/'
+            //                    }]
+            //                }
+            //            },
 
             // Automatically inject Bower components into the app
             wiredep: {
@@ -465,8 +462,7 @@
             // Test settings
             karma: {
                 unit: {
-                    configFile: 'test/karma.conf.js',
-                    singleRun: true
+                    configFile: 'karma.conf.js'
                 }
             },
 
@@ -476,8 +472,7 @@
                         port: 9000,
                         base: '<%= yeoman.app %>',
                         // Change this to '0.0.0.0' to access the server from outside.
-                        hostname: 'localhost'
-                            //livereload: 35729
+                        hostname: 'localhost' //livereload: 35729
                     }
                 }
             }
@@ -488,6 +483,7 @@
             grunt.log.write('Folgende TASK werden angeboten:\n');
             grunt.log.write('* grunt ohne Parameter führt JSLint auf dem Projekt aus.\n');
             grunt.log.write('* grunt startServer: startet den Webserver\n');
+            grunt.log.write('* grunt verify: startet die GUI Tests über Karama\n');
             grunt.log.write('Hilfe ausgegeben: ').ok();
         });
 
@@ -548,6 +544,11 @@
 
         grunt.registerTask('startServer', [
             'connect:myServer:keepalive'
+        ]);
+
+        grunt.registerTask('verify', [
+            'connect:myServer',
+            'karma'
         ]);
     };
 }());
